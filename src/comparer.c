dd32d0b578a97ecfa4baeecced13292b42200663
Иван Азарников <azarnikov01@gmail.com>
2020-12-05 00:01:11 +0300

    bugs fixed

--- src/comparer.c
+++ src/comparer.c
@@ -4,16 +4,32 @@
 
 #define error(...) (fprintf(stderr, __VA_ARGS__))
 
+#define MEMORY_ERROR_CODE -1
+
 int main(int argc, char *argv[]) {
     int bit_count1 = return_bits_per_pixel(argv[1]);
+    if (bit_count1 == FILE_OPEN_ERROR) {
+        return FILE_OPEN_ERROR;
+    }
     int bit_count2 = return_bits_per_pixel(argv[2]);
+    if (bit_count2 == FILE_OPEN_ERROR) {
+        return FILE_OPEN_ERROR;
+    }
     if (bit_count1 != bit_count2) {
-        error("In file %s %d bits per pixel but in file %s %d", argv[1], bit_count1, argv[2], bit_count2);
+        error("In file %s %d bits per pixel but in file %s %d\n", argv[1], bit_count1, argv[2], bit_count2);
         return -1;
     } else {
         if (bit_count1 == 8) {
             BMP_8 *image1 = malloc(sizeof(*image1));
+            if (image1 == NULL) {
+                error("Can not allocate memory for image1\n");
+                return MEMORY_ERROR_CODE;
+            }
             BMP_8 *image2 = malloc(sizeof(*image2));
+            if (image2 == NULL) {
+                error("Can not allocate memory for image1\n");
+                return MEMORY_ERROR_CODE;
+            }
             int loading_result1 = load_bmp_8(image1, argv[1]);
             int loading_result2 = load_bmp_8(image2, argv[2]);
             if (loading_result1 != 0) {
@@ -26,6 +42,8 @@ int main(int argc, char *argv[]) {
             if (compare_result != 0) {
                 return compare_result;
             }
+            free_bmp8(image1);
+            free_bmp8(image2);
         } else if (bit_count1 == 24) {
             BMP_24 *image1 = malloc(sizeof(*image1));
             BMP_24 *image2 = malloc(sizeof(*image2));
@@ -41,6 +59,8 @@ int main(int argc, char *argv[]) {
             if (compare_result != 0) {
                 return compare_result;
             }
+            free_bmp24(image1);
+            free_bmp24(image2);
         }
     }
     return 0;
